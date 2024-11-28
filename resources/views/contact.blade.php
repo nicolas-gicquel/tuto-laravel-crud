83d4bd9d380ee4cdea24ee2007bc76c0b568046c
Nicolas Gicquel <64499780+nicolas-gicquel@users.noreply.github.com>
2024-11-28 16:52:39 +0100

    fix 20241128

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+<h1>Page contact</h1>
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProduitController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::resource('produits', ProduitController::class);
